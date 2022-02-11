a0b0d315e9ec09aca73bae76f9bf35f200b4be7c
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2022-02-11 02:21:24 -0300

    comodo adicionado

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,14 @@
 class Calculadora:
-    area_paredes: float
-    area_teto: float
+    __area_paredes: float ## __UNDERSCORE X2 tem seu acesso limitado , só pode ser acessado por atributos e métodos dentro da sua propria classe.
+    __area_teto: float ## são declarados como privados
 
-    def calcular_area_paredes(self, largura, profundidade,altura):
-        self.area_paredes = (2*(largura + profundidade)* altura)
-        return self.area_paredes
+    def calcular_area_paredes(self, comodo):
+        self.__area_paredes = 2*(comodo.largura + comodo.profundidade)* comodo.altura
+        return self.__area_paredes
 
-    def calcular_area_teto(self,largura,profundidade):
-        self.area_teto = largura * profundidade
-        return self.area_teto
+    def calcular_area_teto(self, comodo):
+        self.__area_teto = comodo.largura * comodo.profundidade
+        return self.__area_teto
     
     def litragem_necessaria(self):
-        return (self.area_paredes * self.area_teto)/10
+        return (self.__area_paredes * self.__area_teto)/10
--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 ##:float = usamos para typar a variavel, no caso ela irá apresentar erro e não aceitar outro tipo de entrada, apenas float
 
 from calculadora import Calculadora
+from comodo import Comodo
 ## criamos o objeto da classe calculadora, instanciando
 calc = Calculadora()
 
-largura: float = float(input('Qual a largura do cômodo? '))
+comodo = Comodo(
+    input('Qual a largura do cômodo? '),
+    input('Qual a profundidade do cômodo? ')
+)
+"""largura: float = float(input('Qual a largura do cômodo? '))
 profundidade: float = float(input('Qual a profundidade do cômodo? '))
-altura: float = 2.9 # pé direito padrão
+altura: float = 2.9 # pé direito padrão"""
 
 
 ##calc.area_paredes= (2*(largura + profundidade)* altura)
-print('A area das paredes tem ',calc.calcular_area_paredes(largura,profundidade,altura),'m²')
+print('A area das paredes tem ',calc.calcular_area_paredes(comodo),'m²')
 
 ##calc.area_teto = (largura * profundidade)
-print('A área do teto tem ', calc.calcular_area_teto(largura, profundidade),'m²')
+print('A área do teto tem ', calc.calcular_area_teto(comodo),'m²')
 
 ##rend_tinta: float = (calc.area_paredes+calc.area_teto)/10
 print('A quantidade de tinta necessária é de',calc.litragem_necessaria(), 'litros.')
